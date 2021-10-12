eb7bcef1c1b5ff81ee7c8e81be95af1c1b50f719
Pedro Coutinho Rimes <pcoutinhorimes@gmail.com>
2021-10-11 22:06:31 -0300

    inicio landing page

---
+++ landing_page.php
@@ -0,0 +1,11 @@
+<?php /* Template Name: Pagina principal */ ?>
+<?php get_header(); ?>
+    <div class="texto_principal">
+        <div id="titulo">
+            Comes&Bebes
+        </div>
+        <div id="sub-titulo">
+            O restaurante para todas as fomes
+        </div>
+    </div>
+<?php get_footer(); ?>
