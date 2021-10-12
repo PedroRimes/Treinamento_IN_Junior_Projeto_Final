ef68ddbcfcaa80887938a0d234837d3494b77fd4
Pedro Coutinho Rimes <pcoutinhorimes@gmail.com>
2021-10-12 20:44:00 -0300

    footer quase completo

--- landing_page.php
+++ landing_page.php
@@ -1,15 +1,15 @@
 <?php /* Template Name: Pagina principal */ ?>
 <?php get_header(); ?>
     <main>
-        <div class="texto_principal">
+        <section class="texto_principal">
             <div id="titulo">
                 Comes&Bebes
             </div>
             <div id="sub-titulo">
                 O restaurante para todas as fomes
             </div>
-        </div>
-        <div class="parte_azul_bb">
+        </section>
+        <section class="parte_azul_bb">
             <div class="texto">
                 CONHEÇA NOSSA LOJA
             </div>
@@ -17,11 +17,31 @@
                 <div class="txttipos">
                     Tipos de pratos principais
                 </div>
-                <div>
-                    <ul class="cardcategoria">
-                        
+                <div class="">
+                    
                 </div>
             </div>
-        </div>
+        </section>
+        <section class="sub-footer">
+            <div class="vnlf">
+                VISITE NOSSA LOJA FÍSICA
+            </div>
+            <div class="end_e_slide">
+                <div class="mapa_end">
+                    <div class="mapa"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3675.1928043754892!2d-43.136297684992726!3d-22.906258343611036!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x99817ee1756031%3A0xd1dcbde0df6f873c!2sAv.%20Milton%20Tavares%20de%20Souza%20-%20Boa%20Viagem%2C%20Niter%C3%B3i%20-%20RJ%2C%2024210-346!5e0!3m2!1spt-BR!2sbr!4v1633111634450!5m2!1spt-BR!2sbr" width="345" height="203" style="border:0;" allowfullscreen="" loading="lazy"></iframe></div>
+                    <div class="endereço">
+                        <img src="<?php echo get_stylesheet_directory_uri() ?>/img/garfoefaca.png" width="19" height="18">
+                        Rua Iorem ipsum, 123, LI, Brasil
+                    </div>
+                    <div class="numero">
+                        <img src="<?php echo get_stylesheet_directory_uri() ?>/img/phone.png" width="19" height="18">
+                        (XX) XXXX-XXXX
+                    </div> 
+                </div>
+                <div class="slide-imagens">
+                <img src="<?php echo get_stylesheet_directory_uri() ?>/img/fotocomendo1.jpg" >
+                </div>
+            </div>
+        </section>
     </main>
 <?php get_footer(); ?>
