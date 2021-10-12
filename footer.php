8e69b20e6acd879cf57d5d71148386fe621d1c1f
Pedro Coutinho Rimes <pcoutinhorimes@gmail.com>
2021-10-12 17:29:54 -0300

    landing page com body

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer id = "pezinho">         
-    <p>© Copyright 2021 - Desenvolvido por Pedro Coutinho Rimes</p> 
+    <p>© Copyright 2021 - Desenvolvido por Pedro Coutinho Rimes</p>  
 </footer> 
 <?php wp_footer(); ?>  
 </body> 
--- landing_page.php
+++ landing_page.php
@@ -1,11 +1,27 @@
 <?php /* Template Name: Pagina principal */ ?>
 <?php get_header(); ?>
-    <div class="texto_principal">
-        <div id="titulo">
-            Comes&Bebes
+    <main>
+        <div class="texto_principal">
+            <div id="titulo">
+                Comes&Bebes
+            </div>
+            <div id="sub-titulo">
+                O restaurante para todas as fomes
+            </div>
         </div>
-        <div id="sub-titulo">
-            O restaurante para todas as fomes
+        <div class="parte_azul_bb">
+            <div class="texto">
+                CONHEÇA NOSSA LOJA
+            </div>
+            <div class="categorias">
+                <div class="txttipos">
+                    Tipos de pratos principais
+                </div>
+                <div>
+                    <ul class="cardcategoria">
+                        
+                </div>
+            </div>
         </div>
-    </div>
+    </main>
 <?php get_footer(); ?>
