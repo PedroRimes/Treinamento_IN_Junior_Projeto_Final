b7f7c134510c8c1602aa9e398fe01f248340b185
Pedro Coutinho Rimes <pcoutinhorimes@gmail.com>
2021-10-16 19:47:43 -0300

    pagina shop iniciando

--- functions.php
+++ functions.php
@@ -28,4 +28,9 @@
         }
     };
     add_action('cart','cb_carrinho');
+    
+    function mytheme_add_woocommerce_support() {
+        add_theme_support( 'woocommerce' );
+    }
+    add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
 ?>
